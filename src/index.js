66e358970c548422842df8618dee5c3b6efa08f7
leminhhung2001 <leminhhungtabletennis@gmail.com>
2021-07-09 18:07:07 +0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./components/Container";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Container />
     </Router>
   </React.StrictMode>,
